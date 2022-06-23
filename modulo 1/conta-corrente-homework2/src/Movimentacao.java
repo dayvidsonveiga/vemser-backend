2b8fa00f75b8ebf55e8a860c005996d08723d03a
dayvidsonveiga <dayvidsonveiga@gmail.com>
2022-06-23 13:36:16 -0300

    Removido abstract dos métodos da interface movimentação.

--- modulo 1/conta-corrente-homework2/src/Movimentacao.java
+++ modulo 1/conta-corrente-homework2/src/Movimentacao.java
@@ -1,5 +1,5 @@
 public interface Movimentacao {
-    public abstract Boolean sacar(double valor);
-    public abstract Boolean depositar(double valor);
-    public abstract Boolean transferir(Conta conta, double valor);
+    public Boolean sacar(double valor);
+    public Boolean depositar(double valor);
+    public Boolean transferir(Conta conta, double valor);
 }
