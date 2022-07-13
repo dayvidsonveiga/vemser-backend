d9d0aeee3cc81c8a35103059641fd717297a6f76
dayvidsonveiga <dayvidsonveiga@gmail.com>
2022-07-13 10:47:38 -0300

    Feat: Homework concluido.

--- modulo 3/pessoaapi/src/main/java/br/com/vemser/pessoaapi/dto/ContatoDTO.java
+++ modulo 3/pessoaapi/src/main/java/br/com/vemser/pessoaapi/dto/ContatoDTO.java
@@ -1,5 +1,6 @@
 package br.com.vemser.pessoaapi.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 public class ContatoDTO extends ContatoCreateDTO {
+
+    @Schema(description = "ID do contato")
     private Integer idContato;
 }
